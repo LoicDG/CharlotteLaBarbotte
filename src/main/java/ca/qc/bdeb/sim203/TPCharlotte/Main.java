0c0e7462b5aa908dfae27216d5264e6f00d7b82c
Loïc <loicd507@gmail.com>
2023-10-26 20:52:00 -0400

    Added ALLO SEBBY

--- src/main/java/ca/qc/bdeb/sim203/TPCharlotte/Main.java
+++ src/main/java/ca/qc/bdeb/sim203/TPCharlotte/Main.java
@@ -2,6 +2,7 @@ package ca.qc.bdeb.sim203.TPCharlotte;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+// ALLO SEBBY
 
 public class Main extends Application {
     public static void main(String[] args) {
