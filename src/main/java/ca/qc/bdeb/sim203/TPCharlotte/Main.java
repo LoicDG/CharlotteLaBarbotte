084201da0581d4e62bd978f45d89fce5a8efc6a0
Loïc <loicd507@gmail.com>
2023-10-27 09:15:04 -0400

    Added Menu Infos

--- src/main/java/ca/qc/bdeb/sim203/TPCharlotte/Main.java
+++ src/main/java/ca/qc/bdeb/sim203/TPCharlotte/Main.java
@@ -1,20 +1,25 @@
 package ca.qc.bdeb.sim203.TPCharlotte;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+import java.util.Random;
 
 public class Main extends Application {
+    public static double HEIGHT = 590;
+    public static double WIDTH = 900;
     public static void main(String[] args) {
         launch(args);
     }
@@ -23,33 +28,87 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Charlotte la Barbotte");
         stage.getIcons().add(new Image("code/charlotte.png"));
-        stage.setResizable(false);
-//Scene 1 : La page titre
-        ImageView logo = new ImageView(new Image("code/logo.png"));
+        stage.setResizable(false);  //Pourquoi setResizable false?
+
+        //region Scène 1, La page titre
+        var rootTitre = new VBox();
+        rootTitre.setAlignment(Pos.CENTER);
+        var titre = new Scene(rootTitre, WIDTH, HEIGHT);
+        var logo = new ImageView(new Image("code/logo.png"));
         logo.setPreserveRatio(true);
         logo.setFitWidth(500);
-        Button jouer = new Button("Jouer");
-        Button infos = new Button("Infos");
-        HBox boutons = new HBox(jouer, infos);
+        var jouer = new Button("Jouer!");
+        var infos = new Button("Infos");
+        var boutons = new HBox(jouer, infos);
         boutons.setAlignment(Pos.CENTER);
-
-        VBox rootTitre = new VBox(logo, boutons);
-        rootTitre.setAlignment(Pos.CENTER);
-
-        Scene titre = new Scene(rootTitre,900,590);
+        boutons.setSpacing(10);
+        rootTitre.getChildren().addAll(logo, boutons);
         rootTitre.setStyle("-fx-background-color: #2A7FFF;");
+        rootTitre.setSpacing(10);
+        //endregion
 
         stage.setScene(titre);
+        //Scène pour le menu informations
+        var rnd = new Random();
+        var ennemiImage = new Image("code/poisson" + rnd.nextInt(1, 6) + ".png");
+        var sceneInfos = setScreenInfos(titre, stage, ennemiImage);
 
-        //Scene 3 : Infos
-        Text nomDuJeu = new Text("Charlotte la Barbotte");
-        nomDuJeu.setFont(Font.font(30));
+        //region Événementiel
+        infos.setOnAction(event -> {
+            //change l'image à chaque fois
+            var poisson = new Image("code/poisson" + rnd.nextInt(1, 6) + ".png");
+            var image = (ImageView) sceneInfos.getRoot().getChildrenUnmodifiable().get(1);
+            image.setImage(poisson);
+            stage.setScene(sceneInfos);
+        });
 
-        //Events!!! Jsp si on devrait mettre tous les events aussi pr que ce soit
-        //plus organise mais lowkey idc, c juste pour la note so cmm idk ce qui
-        //est le plus clean
+        //endregion
 
 
         stage.show();
+    }
+
+    private Scene setScreenInfos(Scene originale, Stage stage, Image poisson) {
+        var root = new VBox();
+        var scene = new Scene(root, WIDTH, HEIGHT);
+        var titre = new Text("Charlotte la Barbotte");
+        titre.setFont(Font.font(48));
+        root.setStyle("-fx-background-color: #2A7FFF;");
+
+        //image poisson
+        var imageEnnemi = new ImageView(poisson);
+        imageEnnemi.setPreserveRatio(true);
+        imageEnnemi.setFitHeight(HEIGHT*0.4);
+
+        //Collaborateurs
+        var collabos = new VBox();
+        var loic = new Text("Par Loïc Desrochers-Girard"); //TODO: arranger le style de "Par" et "et" si on veut
+        loic.setFont(Font.font(32));
+        var sebby = new Text("et Sebastian Crête");
+        sebby.setFont(Font.font(32));
+        collabos.getChildren().addAll(loic, sebby);
+        collabos.setAlignment(Pos.CENTER);
+
+        //Description du travail
+        var sources = new Text("Travail remis à Nicolas Hurtubise et Georges Côté. Graphismes adaptés de " +
+                "https://game-icons.net/ et de\n" +
+                "https://openclipart.org/. Développé dans le cadre du cours 420-203-RE - Développement de programmes \n" +
+                "dans un environnement graphique, au Collège de Bois-de-Boulogne.");
+
+        var boutonRetour = new Button("Retour");
+        root.getChildren().addAll(titre, imageEnnemi, collabos, sources, boutonRetour);
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+        //region Événementiel
+        boutonRetour.setOnAction(event -> {
+            stage.setScene(originale);
+        });
+        scene.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ESCAPE){
+                stage.setScene(originale);
+            }
+        });
+        //endregion
+        return scene;
     }
 }
