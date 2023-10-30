2d2dd237d1cbadbcc0075244478f0212c4fad282
Loïc <loicd507@gmail.com>
2023-10-30 17:19:04 -0400

    Added class Partie, gotta start moving a lot of stuff ot it (voir animations 5)

---
+++ src/main/java/ca/qc/bdeb/sim203/TPCharlotte/Partie.java
@@ -0,0 +1,8 @@
+package ca.qc.bdeb.sim203.TPCharlotte;
+
+import ca.qc.bdeb.sim203.TPCharlotte.Poissons.Charlotte;
+import javafx.scene.image.Image;
+
+public class Partie {
+    private Charlotte charlotte = new Charlotte(new Image("code/charlotte.png"), 0, Main.HEIGHT / 2);
+}
